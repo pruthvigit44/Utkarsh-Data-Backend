a021df8b6189f7c537420b224e4c72f3f3e14685
pruthvigit44 <bhattpruthvi23@gmail.com>
2026-04-22 00:18:58 +0530

    Cors Fixed

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,15 @@ import userRouter from "./routes/user.route";
 
 const app = express();
 
-app.use(cors({ origin: "*" })); // TEMP
+// app.use(cors({ origin: "*" })); // TEMP
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                 // local dev
+    "https://utkarsh-data-frontend.vercel.app/"      // replace with actual URL
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/users", userRouter);
